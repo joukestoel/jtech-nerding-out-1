a58db9497006b263fe16667001261de76f4b86d2
Jouke Stoel <j.h.stoel@gmail.com>
2013-03-08 15:19:56 +0100

    [MAJOR] Initial set up

---
+++ your_own_robot/src/nl/ordina/nerdingout/example/ExampleBot.java
@@ -0,0 +1,11 @@
+package nl.ordina.nerdingout.example;
+
+import robocode.AdvancedRobot;
+
+public class ExampleBot extends AdvancedRobot {
+    public void run() {
+        while(true) {
+            ahead(100);
+        }
+    }
+}
