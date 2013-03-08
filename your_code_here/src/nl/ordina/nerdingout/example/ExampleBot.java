d552ce242ea0f0a2fa5c23f2a7bce2d33ea71614
Martijn Blankestijn <martijnblankestijn@gmail.com>
2013-03-08 15:58:56 +0100

    bijdragen mblankestijn

--- your_code_here/src/nl/ordina/nerdingout/example/ExampleBot.java
+++ your_code_here/src/nl/ordina/nerdingout/example/ExampleBot.java
@@ -1,11 +1,29 @@
 package nl.ordina.nerdingout.example;
 
 import robocode.AdvancedRobot;
+import robocode.ScannedRobotEvent;
 
 public class ExampleBot extends AdvancedRobot {
+
     public void run() {
         while(true) {
-            ahead(100);
+            turnRadarLeft(360);
+
         }
+    }
+
+    @Override
+    public void onScannedRobot(ScannedRobotEvent event) {
+        double bearing = event.getBearing();
+        double heading = event.getHeading();
+        if (bearing > 0) {
+            turnRight(bearing);
+        }
+        else {
+            turnLeft(-bearing);
+        }
+        System.out.println(bearing);
+        setAhead(event.getDistance() / 2);
+        fire(3);
     }
 }
