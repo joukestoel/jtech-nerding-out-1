deadcd509a20865414a5255cd29cc855461202b9
Robert van Rijn <rvanrijn79@gmail.com>
2013-03-22 13:35:38 +0100

    Best bot ever

--- your_code_here/src/nl/ordina/nerdingout/round_2/HareDestroyer.java
+++ your_code_here/src/nl/ordina/nerdingout/round_2/HareDestroyer.java
@@ -1,10 +1,16 @@
 package nl.ordina.nerdingout.round_2;
 
 import robocode.AdvancedRobot;
+import robocode.ScannedRobotEvent;
 
 import java.awt.*;
 
+import static robocode.util.Utils.normalRelativeAngleDegrees;
+
 public class HareDestroyer extends AdvancedRobot {
+
+    private double fireDistance = 300;
+    private double stoppingDistance = 100;
 
 
     public void run() {
@@ -16,4 +22,26 @@ public class HareDestroyer extends AdvancedRobot {
             turnRadarLeft(360);
         }
     }
+
+    @Override
+    public void onScannedRobot(ScannedRobotEvent e) {
+
+
+        double bearing = e.getBearing();
+        //double angle = normalRelativeAngleDegrees((getHeading() - getRadarHeading()) + bearing);
+        setTurnRight(bearing);
+
+
+        if(e.getDistance() > stoppingDistance) {
+            setAhead(50);
+        }
+
+        if(e.getDistance() < fireDistance){
+            setFire(3);
+        }
+
+
+
+    }
+
 }
