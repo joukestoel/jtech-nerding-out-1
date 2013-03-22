2e8476de568b3cc5d604d2d9cc877280a8c35d4e
Arjan Dikhoff <arjan.dikhoff@clockwork.nl>
2013-03-22 15:22:39 +0100

    R2D2 joined the party!

--- your_code_here/src/nl/ordina/nerdingout/round_3/R2D2.java
+++ your_code_here/src/nl/ordina/nerdingout/round_3/R2D2.java
@@ -3,10 +3,5 @@ package nl.ordina.nerdingout.round_3;
 import robocode.AdvancedRobot;
 
 public class R2D2 extends AdvancedRobot {
-    @Override
-    public void run() {
-        while(true) {
-            // start your engines for round 3!
-        }
-    }
+
 }
