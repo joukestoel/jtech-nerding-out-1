3649369fed461e8bfc6348b07dde02536ae6b28d
IvoNet <ivonet@gmail.com>
2013-03-22 13:24:22 +0100

    round two first import

--- your_code_here/src/nl/ordina/nerdingout/round_2/FourtyTwo.java
+++ your_code_here/src/nl/ordina/nerdingout/round_2/FourtyTwo.java
@@ -2,9 +2,12 @@ package nl.ordina.nerdingout.round_2;
 
 import robocode.AdvancedRobot;
 
-public class YourBot extends AdvancedRobot {
+import java.awt.*;
+
+public class FourtyTwo extends AdvancedRobot {
     @Override
     public void run() {
+        setColors(Color.BLACK, Color.BLACK, Color.WHITE);
         while(true) {
             // start your engines for round 2!
         }
