4abcd09234a42232fad2429a8f8905ed301dfa83
Arjan Dikhoff <arjan.dikhoff@clockwork.nl>
2013-03-22 15:13:32 +0100

    lkjlk

--- your_code_here/src/nl/ordina/nerdingout/round_3/R2D2.java
+++ your_code_here/src/nl/ordina/nerdingout/round_3/R2D2.java
@@ -2,7 +2,7 @@ package nl.ordina.nerdingout.round_3;
 
 import robocode.AdvancedRobot;
 
-public class YourBot extends AdvancedRobot {
+public class R2D2 extends AdvancedRobot {
     @Override
     public void run() {
         while(true) {
