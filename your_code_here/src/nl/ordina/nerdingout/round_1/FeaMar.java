e5cd386940574bb4e300334b27d88db65a54db24
IvoNet <ivonet@gmail.com>
2013-03-22 11:31:07 +0100

    Feamar first commit

--- your_code_here/src/nl/ordina/nerdingout/round_1/FeaMar.java
+++ your_code_here/src/nl/ordina/nerdingout/round_1/FeaMar.java
@@ -2,11 +2,14 @@ package nl.ordina.nerdingout.round_1;
 
 import robocode.AdvancedRobot;
 
-public class YourBot extends AdvancedRobot {
+public class FeaMar extends AdvancedRobot {
     @Override
     public void run() {
         while(true) {
             // start your engines for round 1!
+            scan();
+            turnLeft(360);
+            
         }
     }
 }
